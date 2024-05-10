b3643a250d185d58d102743e7a5f871b364bddf0
DryDish <57399961+DryDish@users.noreply.github.com>
2024-05-10 14:45:22 +0200

    add more tests
    add tests for: filter, or, orElse, insert, getOrInsert, getOrInserWith,
    take, takeIf, and replace.

--- tests/option/option.test.ts
+++ tests/option/option.test.ts
@@ -189,4 +189,188 @@ describe("Option method tests", () => {
 			expect(None<number>().andThen((_) => None())).toEqual(None());
 		});
 	});
+
+	describe("Option.filter()", () => {
+		test("Some(2).filter((x) => x > 1) should return Some(2)", () => {
+			expect(Some(2).filter((x) => x > 1)).toEqual(Some(2));
+		});
+		test("Some(1).filter((x) => x > 1) should return None()", () => {
+			expect(Some(1).filter((x) => x > 1)).toEqual(None());
+		});
+		test("None().filter((x) => x > 1) should return None()", () => {
+			expect(None<number>().filter((x) => x > 1)).toEqual(None());
+		});
+	});
+
+	describe("Option.or()", () => {
+		test("Some(2).or(Some(3)) should return Some(2)", () => {
+			expect(Some(2).or(Some(3))).toEqual(Some(2));
+		});
+		test("Some(2).or(None()) should return Some(2)", () => {
+			expect(Some(2).or(None())).toEqual(Some(2));
+		});
+		test("None().or(Some(3)) should return Some(3)", () => {
+			expect(None<number>().or(Some(3))).toEqual(Some(3));
+		});
+		test("None().or(None()) should return None()", () => {
+			expect(None<number>().or(None())).toEqual(None());
+		});
+	});
+
+	describe("Option.orElse()", () => {
+		test("Some(2).orElse(() => Some(3)) should return Some(2)", () => {
+			expect(Some(2).orElse(() => Some(3))).toEqual(Some(2));
+		});
+		test("Some(2).orElse(() => None()) should return Some(2)", () => {
+			expect(Some(2).orElse(() => None())).toEqual(Some(2));
+		});
+		test("None().orElse(() => Some(3)) should return Some(3)", () => {
+			expect(None<number>().orElse(() => Some(3))).toEqual(Some(3));
+		});
+		test("None().orElse(() => None()) should return None()", () => {
+			expect(None<number>().orElse(() => None())).toEqual(None());
+		});
+	});
+
+	describe("Option.insert()", () => {
+		test("Some(2).insert(5) should return 5", () => {
+			const option = Some(1);
+			const value = option.insert(5);
+
+			expect(value).toEqual(5);
+			expect(option).toEqual(Some(5));
+		});
+		test("None().insert(3) should return 3", () => {
+			const option = None<number>();
+
+			expect(option.isSome()).toBe(false);
+
+			const value = option.insert(3);
+
+			expect(value).toEqual(3);
+			expect(option).toEqual(Some(3));
+			expect(option.isSome()).toBe(true);
+		});
+	});
+
+	describe("Option.getOrInsert()", () => {
+		test("Some(2).getOrInsert(5) should return 2", () => {
+			const option = Some(2);
+			const value = option.getOrInsert(5);
+
+			expect(value).toEqual(2);
+			expect(option).toEqual(Some(2));
+		});
+		test("None().getOrInsert(3) should return 3", () => {
+			const option = None<number>();
+
+			expect(option.isSome()).toBe(false);
+
+			const value = option.getOrInsert(3);
+
+			expect(value).toEqual(3);
+			expect(option).toEqual(Some(3));
+			expect(option.isSome()).toBe(true);
+		});
+	});
+
+	describe("Option.getOrInsertWith()", () => {
+		test("Some(2).getOrInsertWith(() => 3) should return 2", () => {
+			const option = Some(2);
+			const value = option.getOrInsertWith(() => 2);
+
+			expect(value).toEqual(2);
+			expect(option).toEqual(Some(2));
+		});
+		test("None().getOrInsertWith(() => 3) should return 3", () => {
+			const option = None<number>();
+
+			expect(option.isSome()).toBe(false);
+
+			const value = option.getOrInsertWith(() => 3);
+
+			expect(value).toEqual(3);
+			expect(option).toEqual(Some(3));
+			expect(option.isSome()).toBe(true);
+		});
+	});
+
+	describe("Option.take()", () => {
+		test("Some(2).take() should return Some(2)", () => {
+			const option = Some(2);
+			const takenOption = option.take();
+
+			expect(option.isSome()).toBe(false);
+			expect(takenOption.isSome()).toBe(true);
+
+			expect(option).toEqual(None());
+			expect(takenOption).toEqual(Some(2));
+		});
+		test("None().take() should return None()", () => {
+			const option = None<number>();
+			const takenOption = option.take();
+
+			expect(option.isSome()).toBe(false);
+			expect(takenOption.isSome()).toBe(false);
+
+			expect(option).toEqual(None());
+			expect(takenOption).toEqual(None());
+		});
+	});
+
+	describe("Option.takeIf()", () => {
+		test("Some(2).takeIf((x) => x > 1) should return Some(2)", () => {
+			const option = Some(2);
+			const takenOption = option.takeIf((x) => x > 1);
+
+			expect(option.isSome()).toBe(false);
+			expect(takenOption.isSome()).toBe(true);
+
+			expect(option).toEqual(None());
+			expect(takenOption).toEqual(Some(2));
+		});
+		test("Some(2).takeIf((x) => x < 1) should return None()", () => {
+			const option = Some(2);
+			const takenOption = option.takeIf((x) => x < 1);
+
+			expect(option.isSome()).toBe(true);
+			expect(takenOption.isSome()).toBe(false);
+
+			expect(option).toEqual(Some(2));
+			expect(takenOption).toEqual(None());
+		});
+		test("None().takeIf((x) => x > 1) should return None()", () => {
+			const option = None<number>();
+			const takenOption = option.takeIf((x) => x > 1);
+
+			expect(option.isSome()).toBe(false);
+			expect(takenOption.isSome()).toBe(false);
+
+			expect(option).toEqual(None());
+			expect(takenOption).toEqual(None());
+		});
+	});
+
+	describe("Option.replace()", () => {
+		test("Some(2).replace(5) should return Some(2)", () => {
+			const option = Some(2);
+			const oldOption = option.replace(5);
+
+			expect(option.isSome()).toBe(true);
+			expect(oldOption.isSome()).toBe(true);
+
+			expect(option).toEqual(Some(5));
+			expect(oldOption).toEqual(Some(2));
+		});
+		test("None().replace(5) should return None()", () => {
+			const option = None<number>();
+			const oldOption = option.replace(5);
+
+			expect(option.isSome()).toBe(true);
+			expect(oldOption.isSome()).toBe(false);
+
+			expect(option).toEqual(Some(5));
+			expect(oldOption).toEqual(None());
+		});
+	});
 });
